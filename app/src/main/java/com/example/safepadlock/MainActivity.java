7bc825a501801d054e8bdd3af58a245a47edde94
Stefani <you@example.com>
2023-10-15 20:21:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/safepadlock/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.safepadlock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Looper;
+
+import java.util.logging.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/safepadlock/loginActivity.java
@@ -0,0 +1,24 @@
+package com.example.safepadlock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class loginActivity extends AppCompatActivity {
+private EditText UserEdt, passEdt;
+private Button loginBTn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+    private void setVariable() {
+
+    }
+    private void initView(){
+       userEdt=findViewById
+    }
+
+}
